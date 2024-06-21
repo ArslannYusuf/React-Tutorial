ee8e84f95f5bfa3265de42d1c00b81944dc2999b
Yusuf Arslan <yeed060118@gmail.com>
2024-06-21 03:08:07 +0300

    users update

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,54 @@ const BASE_URL = 'http://localhost:3005';
 function App() {
 
 
-/*     const getAllUsers = async () =>{
+    const getAllUsers = async () =>{
     const response =  await axios.get(BASE_URL + "/users");
 
     console.log(response.data[0]);
-    } */
+    }
+
+
 
     const getUserById = async(userId)=>{
       const response = await axios.get(`${BASE_URL}/users/${userId}`);
-
+  
       console.log(response.data);
     }
 
+
+    const createUser = async(newUser)=>{
+      const response =  await axios.post(`${BASE_URL}/users`,newUser)
+
+      console.log("response",response.data);
+    }
+
+
+    const updateUser = async(userId,updatedUser)=>{
+      const response =  await axios.put(`${BASE_URL}/users/${userId}`,updatedUser)
+
+      console.log("response",response.data);
+    }
+
+
     useEffect(()=>{
      // getAllUsers();
-     getUserById(1);
+     // getUserById(1);
+
+     /* const newUser = {
+       "username":"ali",
+       "password":"789"
+     }
+     createUser(newUser); */
+
+
+
+   /*   updateUser(8891,{
+       "username":"veli",
+       "password":"yyy" 
+     }); */
+
+     
+     
     },[])
 
   return (
