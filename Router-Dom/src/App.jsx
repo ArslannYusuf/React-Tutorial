bed63eee28f268979534ecdeb49654d82db33541
Yusuf Arslan <yeed060118@gmail.com>
2024-06-26 01:59:30 +0300

    react-router-dom

--- Router-Dom/src/App.jsx
+++ Router-Dom/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Products from './pages/Products'
+import Header from './components/Header'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
-      
+      <Header/>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='*' element={<NotFoundPage />} />
+      </Routes>
     </div>
   )
 }
