cf2661809f1995338d67c1294a2ca40c03ad07f6
Yusuf Arslan <yeed060118@gmail.com>
2024-06-26 01:18:21 +0300

    update

--- Redux Toolkit/src/App.jsx
+++ Redux Toolkit/src/App.jsx
@@ -1,34 +1,33 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/counterSlice';
-import UserList from './UserList';
-
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counterSlice";
+import UserList from "./UserList";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-  const {value} = useSelector((store) => store.counter);
+  const { value } = useSelector((store) => store.counter);
   console.log(value);
 
-  const dispatch  = useDispatch();
-
-
+  const dispatch = useDispatch();
 
   return (
-
-      <>
+    <>
       {/* dispatch ile actioni tetikleyebiliriz */}
       <div>
-      <div>{value}</div>
-      <div><button onClick={() => dispatch(increment())}>Arttır</button> <button onClick={() => dispatch(decrement())}>Azalt</button></div>
-    </div>
+        <div>{value}</div>
+        <div>
+          <button onClick={() => dispatch(increment())}>Arttır</button>{" "}
+          <button onClick={() => dispatch(decrement())}>Azalt</button>
+        </div>
+      </div>
 
-    {/* createAsyncThunk ile HTTP istegi gonderdim */}
-    <div>
-      <UserList/>
-    </div>
-      </>
-  )
+      <hr />
+      {/* createAsyncThunk ile HTTP istegi gonderdim */}
+      <div>
+        <UserList />
+      </div>
+      <hr />
+    </>
+  );
 }
 
-export default App
+export default App;
