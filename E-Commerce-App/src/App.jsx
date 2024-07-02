0923aac13429edd65fa24c6709e3a8962fb68992
Yusuf Arslan <yeed060118@gmail.com>
2024-07-03 02:46:30 +0300

    basket

--- E-Commerce-App/src/App.jsx
+++ E-Commerce-App/src/App.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import './App.css'
-import PageContainer from './container/PageContainer'
-import Header from './components/Header'
-
-import RouterConfig from './config/RouterConfig'
-import Loading from './components/Loading'
-
+import React from "react";
+import "./App.css";
+import PageContainer from "./container/PageContainer";
+import Header from "./components/Header";
+import Drawer from "@mui/material/Drawer";
+import RouterConfig from "./config/RouterConfig";
+import Loading from "./components/Loading";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { products } = useSelector((store) => store.basket);
 
   return (
     <div>
       <PageContainer>
         <Header />
-        <RouterConfig/>
+        <RouterConfig />
         <Loading />
+        <Drawer open={true} className="drawer" anchor="right">
+          {products &&
+            products.map((product) => {
+              return (
+                <div className="flex-row" style={{padding: '25px'}}>
+                  <img src={product.image} width={50} height={50} style={{ marginRight: '10px'}}/>
+                  <p style={{ width: '320px'}} className="app-product-title">{product.title} ({product.count})</p>
+                  <p>{product.price}</p>
+                </div>
+              );
+            })}
+        </Drawer>
       </PageContainer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
